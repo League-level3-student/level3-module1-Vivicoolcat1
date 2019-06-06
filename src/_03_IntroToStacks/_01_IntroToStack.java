609b2dcee618f00df5fe69996921e469ae30a22a
League Student <league@WTS14-4.attlocal.net>
2019-06-05 19:50:57 -0700

    :smile:

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,15 +9,43 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> num = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		Random r = new Random();
+		for (int i = 0; i <=100; i++) {
+			double r2= r.nextDouble()*100;
+			num.push(r2);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String answer= JOptionPane.showInputDialog("Enter in a number between 0 and 100");
+		String answer2= JOptionPane.showInputDialog("Enter in another number between 0 and 100");
+		double num1 = Double.parseDouble(answer);
+		double num2 =  Double.parseDouble(answer2);;
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		double largest =0;
+		double lowest = 0;
+		System.out.println("Popping elements off stack...");
 		
+		while(!num.isEmpty()) {
+		double x= num.pop();
+			
+		if(num1<num2) {
+			largest = num2;
+			lowest = num1;
+		}
+		else {
+			largest = num1;
+			lowest = num2;
+		}
 		
+		if(x<largest && x>lowest ) {
+			System.out.println("Elements between " + lowest + " and " + largest + ":" + "\n"+ x);
+		}
+		else {
+			System.out.println("not");
+		}
+		}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
