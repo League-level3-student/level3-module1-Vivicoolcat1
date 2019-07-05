9710ef237a77bd5d5bf517e2328d178f795316a1
League Student <league@tests-iMac.attlocal.net>
2019-07-05 16:26:10 -0700

    :smile:

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -7,11 +9,14 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan{
+public class HangMan implements KeyListener {
 	JFrame frame;
 	 JPanel panel;
 	 JLabel label1;
 	 Stack <String> hangmanwords = new Stack <String>();
+	 String wordpop ="";
+	 String underscore = "";
+	 int lives = 3;
 	public static void main(String[] args) {
 		HangMan man = new HangMan ();
 		man.start();
@@ -24,24 +29,62 @@ public class HangMan{
 	frame.add(panel);
 	panel.add(label1);
 	frame.setVisible(true);
-	
+	frame.addKeyListener(this);
 	String howmany =JOptionPane.showInputDialog("Pick how many words you want to do up to 266");
 	int howmany2 =Integer.parseInt(howmany);
 String word =	Utilities.readRandomLineFromFile("dictionary.txt");
 
 	for (int i = 0; i < howmany2; i++) {
 		hangmanwords.push(word);
-		System.out.println("hi");
+		
 		
 	}
-	String wordpop =hangmanwords.pop();
+	wordpop= hangmanwords.pop();
+	System.out.println(wordpop);
+	
 	for (int i = 0; i < wordpop.length(); i++) {
-		blank = blank + blank;
+	underscore += blank;
+	
 	}
-
-	label1.setText(blank);
+label1.setText(underscore);
 
 	}
 	
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
 	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+
+	if(wordpop.contains(e.getKeyChar() + "")) {
+		String temp = "";
+		for (int i = 0; i < wordpop.length(); i++) {
+			if(wordpop.charAt(i) == e.getKeyChar()) {
+				temp += e.getKeyChar();
+			}
+			else {
+				temp += underscore.charAt(i);
+				
+			}
+		} underscore = temp;
+		label1.setText(underscore);
+	}
+	else {
+	lives=lives-1;
+	JOptionPane.showMessageDialog(null, "Wrong! you have " + lives + " lives/ life left");
+	if(lives == 0) {
+		JOptionPane.showMessageDialog(null, "Game over");
+	}
+	}
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
 }
